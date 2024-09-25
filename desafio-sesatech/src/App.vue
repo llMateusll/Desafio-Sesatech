cd6f7d37dd3931a5e2c0d8cd9532d43f678e506d
llMateusll <mateus.d.santana.99@gmail.com>
2024-09-25 18:23:07 -0300

    Inicio do desafio sesatech

---
+++ desafio-sesatech/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <img alt="Vue logo" src="./assets/Sesa-fundo-branco.png">
+  <login/>
+</template>
+
+<script>
+import login from './components/login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
+
---
+++ desafio-sesatech/src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h2 style="margin: 20px;">Login</h2>
+        <form @submit.prevent="handleLogin">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" style="margin: 10px;" id="name" v-model="name"
+              placeholder="Enter your name" required />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" style="margin: 10px;" class="form-control" id="password" v-model="password"
+              placeholder="Enter your password" required />
+          </div>
+          <div class="form-group">
+            <button type="submit" style="margin: 10px;" class="btn btn-success ">Login</button>
+          </div>
+          <p v-if="errorMessage" class="text-danger">{{ errorMessage }}</p>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
